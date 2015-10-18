c59627b138e804490486d9caed717ab22ef7252b
Zora <MrZoraman@Gmail.com>
2015-10-17 21:45:29 -0600

    Final documentation. Also changed an access modifier.

--- src/main/java/com/lagopusempire/lagoprops/resources/ResourceExporter.java
+++ src/main/java/com/lagopusempire/lagoprops/resources/ResourceExporter.java
@@ -7,15 +7,33 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 
+/**
+ * An internal resource exporter. Gets files from inside the jar and either
+ *      reads them or copies them to the disk.
+ * @author Matt
+ */
 public class ResourceExporter
 {
     private final Class c;
     
+    /**
+     * Constructor.
+     * @param c The class is a part of the jar containing the resource to be
+     *      exported.
+     */
     public ResourceExporter(Class c)
     {
         this.c = c;
     }
     
+    /**
+     * Exports the resource to disk.
+     * @param resource The name of the resource in the jar (and on the disk)
+     * @throws IOException If something goes wrong writing to disk or reading
+     *      from the jar.
+     * @throws ResourceNotFoundException if the resource cannot be found in the
+     *      jar file.
+     */
     public void exportResource(String resource) throws IOException
     {
         File file = new File(resource);
@@ -30,6 +48,17 @@ public class ResourceExporter
         }
     }
     
+    /**
+     * Just gets an input stream of the resource in the jar file.
+     *      It is the caller's responsibility to close the stream when they are
+     *      done with it.
+     * @param resource The name of the resource in the jar.
+     * @return The inputstream for the resource.
+     * @throws IOException If something goes wrong reading the resource from the
+     *      jar.
+     * @throws ResourceNotFoundException if the resource cannot be found in the
+     *      jar file.
+     */
     public InputStream getResourceStream(String resource) throws IOException
     {
         final URL resourceUrl = c.getClassLoader().getResource(resource);
--- src/main/java/com/lagopusempire/lagoprops/resources/ResourceNotFoundException.java
+++ src/main/java/com/lagopusempire/lagoprops/resources/ResourceNotFoundException.java
@@ -1,8 +1,13 @@
 package com.lagopusempire.lagoprops.resources;
 
+/**
+ * This exception is thrown if LagoProps looks for a resource in a jar and it
+ *      cannot find said resource.
+ * @author Zora
+ */
 public class ResourceNotFoundException extends RuntimeException
 {
-    public ResourceNotFoundException(String message)
+    ResourceNotFoundException(String message)
     {
         super(message);
     }
