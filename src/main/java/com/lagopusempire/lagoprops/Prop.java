9ca33f96c9a6b283474c07ba1f3af0f278311dab
Zora <MrZoraman@Gmail.com>
2015-10-17 21:37:38 -0600

    Documentation

--- src/main/java/com/lagopusempire/lagoprops/Prop.java
+++ src/main/java/com/lagopusempire/lagoprops/Prop.java
@@ -1,5 +1,11 @@
 package com.lagopusempire.lagoprops;
 
+/**
+ * Represents a property that can be read from and written to.
+ * You should never construct this class yourself, but rather let an implementation of {@link LagoProps} get it for you.
+ * @author MrZoraman
+ * @param <T> This is the type of data stored by this property. So far valid options are INTEGER, DOUBLE and STRING.
+ */
 public class Prop<T>
 {
     private Prop(LagoProps props, String key, T data, boolean isNull, boolean isError, String originalData)
@@ -34,16 +40,30 @@ public class Prop<T>
     private boolean isError;
     private final String originalData;
     
+    /**
+     * Get the data stored by this property.
+     * Be sure to check some of the other flags to make sure it is ok to use!
+     * @return The data, if reading was successful.
+     */
     public T get()
     {
         return data;
     }
     
+    /**
+     * Checks if the data was null or not.
+     * @return True if nothing was found for the given key, false if something was found.
+     */
     public boolean isNull()
     {
         return isNull;
     }
     
+    /**
+     * Sets a new value for this property.
+     * @param value The value to save to the config file. Changes won't take
+     *      until {@link LagoProps#save()} is called.
+     */
     public void set(T value)
     {
         if(value == null)
@@ -57,11 +77,22 @@ public class Prop<T>
         data = value;
     }
     
+    /**
+     * Checks if there was an error reading or not.
+     * @return true of there was a failure reading the data. This is likey
+     *      cause by a failure to parse a number.
+     */
     public boolean isError()
     {
         return isError;
     }
     
+    /**
+     * This is for debug and feedback purposes. Gets the data that LagoProps
+     *      found, if any.
+     * @return The original data that is in the file itself. It may be valid,
+     *      but it may not be.
+     */
     public String originalData()
     {
         return originalData;
